20745d7d7d64792adf9a7b8c6721db93bef8ca1c
Joseph Kandi <joseph@apps4u.co.za>
2017-05-09 16:04:00 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/peruzal/popularmovies/PopuarMoviesInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.peruzal.popularmovies;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class PopuarMoviesInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.peruzal.popularmovies", appContext.getPackageName());
+    }
+}
